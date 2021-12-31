06318ecdfbb2e203cb165ad0ffe06f4f64fdfc8b
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2021-12-30 19:44:22 -0800

    Implemented Copyright component

---
+++ src/components/CopyRight/CopyRight.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './CopyRight.css';
+
+function CopyRight() {
+
+    return (
+      <>
+      <div className='copyright'>
+          <p>Copyright © 2021 SYPTECH - All rights reserved</p>
+      </div>
+      </>
+    );
+  }
+  
+  export default CopyRight;
