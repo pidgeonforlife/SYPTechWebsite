cafb585bd7d791b72c36c1b342cafd36f73dfc43
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2022-01-02 20:10:14 -0800

    Handled routing for Services page

---
+++ src/pages/Services.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar"
+import Footer from "../components/Footer/Footer"
+import CopyRight from "../components/CopyRight/CopyRight"
+
+function Services() {
+  return (
+    <div>
+        <Navbar />
+        <Footer />
+        <CopyRight />
+    </div>
+  );
+}
+
+export default Services;
